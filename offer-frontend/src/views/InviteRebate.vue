887087913a42da05c9ba6b07f852b672a7fd0578
Rock Lin <2397812547@qq.com>
2025-09-15 18:53:22 +0800

    fix bug：提现图标修改

--- offer-frontend/src/views/InviteRebate.vue
+++ offer-frontend/src/views/InviteRebate.vue
@@ -26,7 +26,7 @@
                 class="px-6 py-3 border border-yellow-300 text-yellow-800 bg-yellow-300 rounded-lg hover:bg-yellow-400 transition-colors flex items-center">
                 <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M17 9V7a2 2 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z">
+                    d="M17 9V7a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2m2 4h10a2 2 0 002-2v-6a2 2 0 00-2-2H9a2 2 0 00-2 2v6a2 2 0 002 2zm7-5a2 2 0 11-4 0 2 2 0 014 0z">
                   </path>
                 </svg>
                 提现
